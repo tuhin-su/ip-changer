7e174f6ed5df8c55a5ae13619e7f8cb3178dcfdb
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-04-08 15:33:03 +0000

    Update base

--- change_tor_ip.sh
+++ change_tor_ip.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# check tor log exist or not
-if [ ! -d $HOME/.local/logs ]; then
-    mkdir -p $HOME/.local/logs
-fi
-
 # Read auth cookie as hex string
 COOKIE=$(xxd -ps /var/run/tor/control.authcookie | tr -d '\n')
 
@@ -13,4 +7,4 @@ printf 'AUTHENTICATE %s\r\nSIGNAL NEWNYM\r\nQUIT\r\n' "$COOKIE" | nc 127.0.0.1 9
 
 # Store IP logs
 IP=$(curl -s --socks5-hostname 127.0.0.1:9050 https://check.torproject.org/api/ip | jq -r .IP)
-echo "{'date':\'$(date '+%Y-%m-%d %H:%M:%S')\', - 'ip': '$IP'}" >> $HOME/.local/logs/tor_ip_log.log
+echo "{'date':\'$(date '+%Y-%m-%d %H:%M:%S')\', - 'ip': '$IP'}" >> /var/log/tor-changer/tor_ip_log.log
--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 TOR_GROUP="debian-tor"
+
+if [ ! -d  /var/log/tor-changer ]; then
+    sudo mkdir -p  /var/log/tor-changer
+fi
+
+sudo chown $USER:$TOR_GROUP /var/log/tor-changer
 sudo apt update && sudo apt install -y curl tor jq xxd
 sudo mkdir /opt/tor
 sudo mv -v change_tor_ip.sh /opt/tor
