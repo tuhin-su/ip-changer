1b8f4488c4f1cafa2d47997fb25a2643f7c7c5c0
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-04-08 15:41:43 +0000

    Update base

--- change_tor_ip.sh
+++ change_tor_ip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Read auth cookie as hex string
 COOKIE=$(xxd -ps /var/run/tor/control.authcookie | tr -d '\n')
-
+formate='+%Y-%m-%d %H:%M:%S'
 # Send NEWNYM signal to Tor ControlPort using cookie authentication
 printf 'AUTHENTICATE %s\r\nSIGNAL NEWNYM\r\nQUIT\r\n' "$COOKIE" | nc 127.0.0.1 9051 > /dev/null
 
 # Store IP logs
 IP=$(curl -s --socks5-hostname 127.0.0.1:9050 https://check.torproject.org/api/ip | jq -r .IP)
-echo "{'date':\'$(date '+%Y-%m-%d %H:%M:%S')\', - 'ip': '$IP'}" >> /var/log/tor-changer/tor_ip_log.log
+echo "{'date':'$(date $formate )', - 'ip': '$IP'}" >> /var/log/tor-changer/tor_ip_log.log
